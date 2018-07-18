77247f2368eb27e12d79a103bfaa42e92b8271d7
jurentie <jurentie@rams.colostate.edu>
2018-07-18 12:57:43 -0600

    allow user to set data variables

--- site/js/fileparser.js
+++ site/js/fileparser.js
@@ -1,7 +1,34 @@
 class FileParser {
-	constructor(){
+	constructor(variableArray = [null, null, null]){
+		this.datevar = variableArray[0] != null ? variableArray[0] : null;
+		this.var1 = variableArray[1] != null ? variableArray[1] : null;
+		this.var2 = variableArray[2] != null ? variableArray[2] : null;
 		this.csv;
 		this.json;
+	}
+
+	setDateVariable(variable){
+		this.datevar = variable;
+	}
+
+	getDateVariable(){
+		return this.datevar;
+	}
+
+	setFirstVariable(variable){
+		this.var1 = variable;
+	}
+
+	getFirstVariable(){
+		return this.var1;
+	}
+
+	setSecondVariable(variable){
+		this.var2 = variable;
+	}
+
+	getSecondVariable(){
+		return var2;
 	}
 
 	csvToJson(filename) {
@@ -21,26 +48,31 @@ class FileParser {
 					dynamicTyping:true,
 					skipEmptyLines:true,
 					complete: function(data){
-						that.json = convertData(data);
+						that.json = convertData(data, that);
 					}
 				})
 			}
 		})
 		
-		function convertData(json){
+		function convertData(json, that){
+			console.log(that.datevar)
+			console.log(that.var1)
+			console.log(that.var2)
 			var firstValue = json.data[0]
-			var year = firstValue["Year"];
+			var year = firstValue[that.datevar];
 			var ret = {"data":[]};
 			var data = ret["data"];
 			data[year] = {};
 			json.data.forEach(function(row){
-				var currdate = row["Year"];
+				var currdate = row[that.datevar];
 				if(year == currdate){
-					data[year][row["County"].trim()] = row["Population"];
+					console.log(row)
+					console.log(row[that.var1])
+					data[year][row[that.var1].trim()] = row[that.var2];
 				}else{
 					year = currdate;
 					data[year] = {};
-					data[year][row["County"].trim()] = row["Population"];
+					data[year][row[that.var1].trim()] = row[that.var2];
 				}
 			})
 			return ret;
